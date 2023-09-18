4264e770534907d60d29046f2ab3615141982552
Troff <troffibp@gmail.com>
2023-09-18 13:28:29 +0300

    chore: continue migrate to next13 and redesign - finish mobile design

--- hospital-next/src/app/components/Carousel/Carousel.tsx
+++ hospital-next/src/app/components/Carousel/Carousel.tsx
@@ -29,9 +29,9 @@ export default function CarouselCustom() {
             slidesToSlide: 2, // optional, default to 1.
         },
         mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 3,
-            slidesToSlide: 2, // optional, default to 1.
+            breakpoint: { max: 500, min: 0 },
+            items: 2,
+            slidesToSlide: 1, // optional, default to 1.
         },
     };
     return (
