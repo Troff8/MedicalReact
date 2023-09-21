1a60a3591cfe5981a7c1c98eaa8d754e2b802708
Troff <troffibp@gmail.com>
2023-09-21 17:13:17 +0300

    feat(Select): add select

--- hospital-next/src/app/components/Select/Select.tsx
+++ hospital-next/src/app/components/Select/Select.tsx
@@ -1,18 +1,14 @@
+'use client';
 import React from 'react';
+import Select from 'react-select';
 
+interface SelectCustomProps {
+    isMulti?: boolean;
+    options: any;
+}
 
-const Select: React.FC = () => {
-    return (
-        <select name="direction">
-            <option value="">Выбор направления</option>
-            <option value="dog">Dog</option>
-            <option value="cat">Cat</option>
-            <option value="hamster">Hamster</option>
-            <option value="parrot">Parrot</option>
-            <option value="spider">Spider</option>
-            <option value="goldfish">Goldfish</option>
-        </select>
-    );
+const SelectCustom: React.FC<SelectCustomProps> = ({ isMulti, options }) => {
+    return <Select options={options} isMulti={isMulti} />;
 };
 
-export default Select;
+export default SelectCustom;
